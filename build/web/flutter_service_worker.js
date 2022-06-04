622895071e3a7431f2632641ef76c5968ef6ffde
Dawid Oliveira <dawidoliveira1@gmail.com>
2022-06-04 01:08:57 -0300

    # generate build web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "38f05373160df1262dedf07926387a41",
-"/": "38f05373160df1262dedf07926387a41",
-"main.dart.js": "e9e5d1458d48f792936b6fd6c2b061e6",
+"index.html": "c39e7ecbf35cd690ae387c70f8b797e9",
+"/": "c39e7ecbf35cd690ae387c70f8b797e9",
+"main.dart.js": "1be0506f3c8ce7f6b64b6dcf1cba8c2d",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "009c9e65172e010890f7f65fde438006"
 };
